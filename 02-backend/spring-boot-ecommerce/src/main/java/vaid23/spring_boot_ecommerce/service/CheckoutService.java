b703a7da4ed3be511c1691bd777b7163c18c6be1
Vaid-Aaryan <ansuvaid@gmail.com>
2026-02-27 19:59:42 +0530

    Handled Payment Processing through stripe in backend by creating and sending PaymentIntent.

--- 02-backend/spring-boot-ecommerce/src/main/java/vaid23/spring_boot_ecommerce/service/CheckoutService.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/vaid23/spring_boot_ecommerce/service/CheckoutService.java
@@ -1,9 +1,14 @@
 package vaid23.spring_boot_ecommerce.service;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import vaid23.spring_boot_ecommerce.dto.PaymentInfo;
 import vaid23.spring_boot_ecommerce.dto.Purchase;
 import vaid23.spring_boot_ecommerce.dto.PurchaseResponse;
 
 public interface CheckoutService {
 
     PurchaseResponse placeOrder(Purchase purchase);
+
+    PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException;
 }
